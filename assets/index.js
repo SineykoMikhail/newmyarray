57d892557a4b2687d8274218ae869ca8f2e39d8b
Sineyko Mikhail <sineykomikhail@gmail.com>
2022-03-27 18:45:35 +0300

    Add pop

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,22 @@
 class MyArray {
-  isMaArray(elem) {
+  constructor() {
+    this.length = 0;
+  }
+  static isMaArray(elem) {
     return elem instanceof MyArray;
   }
+  push(...elem) {
+    for (let i = 0; i < elem.length; i++) {
+      this[this.length++] = elem[i];
+    }
+    return this.length;
+  }
+  pop() { 
+    if }
 }
+
 const str1 = "hello world";
 const arr = new MyArray();
 
-console.log(MyArray.prototype.isMaArray(str1));
-console.log(MyArray.prototype.isMaArray(arr));
-
-
+console.log(MyArray.isMaArray(str1));
+console.log(MyArray.isMaArray(arr));
